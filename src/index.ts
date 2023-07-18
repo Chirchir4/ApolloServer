7386fb5f3c9fb64732ef6e6adbad0244a9c1d7ef
Chirchir4 <gloriachirchir4@gmail.com>
2023-07-18 22:36:44 +0300

    link postgres db with server

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 
 import {typeDefs} from "../src/graphql/schema.js"
 import {resolvers}from "../src/graphql/resolvers.js"
-
+import {client} from "../database/mockdb.js"
+client.connect((err) => {
+  client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    client.end()
+  })
+})
 const server = new ApolloServer({
     typeDefs,
     resolvers,
